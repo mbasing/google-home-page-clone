1bc74dbd4e2b26b59b7c1c188c2aed4aaa98dc05
Moses Basing <mosesbasing@gmail.com>
2021-07-23 18:58:05 +0000

    Change href value to /

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,20 @@ const Header = () => {
     <div className="header">
       <ul>
         <li>
-          <a href="dummy">Gmail</a>
+          <a href="/">Gmail</a>
         </li>
         <li>
-          <a href="dummy">Images</a>
+          <a href="/">Images</a>
         </li>
         <li class="circular-background" style={{ paddingTop: "1.6rem" }}>
-          <a href="dummy">
+          <a href="/">
             <svg class="dots" focusable="false" viewBox="0 0 24 24">
               <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
             </svg>
           </a>
         </li>
         <li className="circular-background" style={{ paddingTop: "1.2rem" }}>
-          <a href="dummy">
+          <a href="/">
             <img src={initial} alt="" />
           </a>
         </li>
